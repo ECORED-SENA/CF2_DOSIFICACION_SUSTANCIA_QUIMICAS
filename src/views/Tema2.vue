51af8bda92331206a9a37957c39fb787aace3ab6
giorgio1966 <jhaycald@hotmail.com>
2022-03-08 22:14:26 -0500

    Segundo avance
    Segundo avance

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -16,13 +16,29 @@
       .bloque-texto-g__texto.p-4
         p.mb-0 Mediante el uso de la prueba de jarras se determinarán las dosis de químicos necesarias para la potabilización del agua. El pH juega un papel muy importante para determinar los fenómenos de coagulación y floculación, dado que el pH debe estar dentro de la zona que corresponde al mínimo de solubilidad de los iones metálicos del coagulante que se haya utilizado.
     p.my-4 El PH desempeña un papel muy importante en el estudio de los fenómenos de coagulación-floculación, preferiblemente el PH debe quedar dentro de la zona correspondiente al mínimo de solubilidad de los iones metálicos del coagulante usado. La temperatura también es un factor que influye bastante, ya que según disminuye la temperatura del agua deberá aumentarse la dosis de productos químicos utilizados como coagulante para obtener unos flóculos adecuados. 
-    .row.justify-content-center
-      .col-lg-4.col-md-12
-        img.my-4(src='@/assets/curso/tema2/imagen2.jpg', alt='Análisis de resultados agua cruda')
-      .col-lg-4.col-md-12
-        img.my-4(src='@/assets/curso/tema2/imagen3.jpg', alt='Análisis de resultados agua cruda')
-      .col-lg-4.col-md-12
-        img.my-4(src='@/assets/curso/tema2/imagen4.jpg', alt='Análisis de resultados agua cruda')
+
+    .row.mb-5.justify-content-center
+      .col-sm-6.col-xl-4.mb-4.mb-xl-0
+        .tarjeta.tarjeta-slide.arriba.color-primario(@mouseover="indicadorTarjetaSlide = false")
+          .indicador--hover(v-if="indicadorTarjetaSlide")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__contenido.p-4.p-xl-5
+              p El tiempo de mezclado del coagulante en el agua que se va a tratar será el necesario para que el producto coagulante que se vaya a utilizar se disuelva con la mayor brevedad posible. 
+            .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/tema2/imagen2.jpg')})`}")
+
+      .col-sm-6.col-xl-4.mb-4.mb-xl-0
+        .tarjeta.tarjeta-slide.abajo.color-secundario(@mouseover="indicadorTarjetaSlide = false")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/tema2/imagen3.jpg')})`}")
+            .tarjeta-slide__contenido.p-4.p-xl-5
+              p Teniendo en cuenta que el tiempo de coagulación es menos de un segundo se debe tener certeza que la neutralización sea total antes de añadir el coagulante. 
+
+      .col-sm-6.col-xl-4.mb-4.mb-sm-0
+        .tarjeta.tarjeta-slide.derecha.color-acento-contenido(@mouseover="indicadorTarjetaSlide = false")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/tema2/imagen4.jpg')})`}")
+            .tarjeta-slide__contenido.p-4.p-xl-5
+              p Para la evaluación de los resultados de una prueba de jarras no existen unos criterios los cuales deban ser seguidos estrictamente.
 
     .row.justify-content-center
       .col-6.fondo2
@@ -52,6 +68,44 @@
         .cajon.fondo3.p-4.mb-4.color-primario
           p Este referente ha sido publicado desde 1905 y desde entonces se actualiza constantemente, hoy en día va en su edición número 23. Esta es una publicación producida con recursos y conocimiento conjunto de las 3 asociaciones de salud pública y agua, reconocidas a nivel mundial: La Asociación Americana de la Salud Pública (APHA), Asociación Americana de trabajos del Agua (AWWA) y La Federación del ambiente agua (WEF).
         p.my-4 El <em>Standard Methods</em> contiene más de 400 métodos para el análisis de agua, debidamente revisados y aprobados por expertos. Estos métodos están divididos en 10 apartados que van desde el apartado 1000 al 10000.
+
+    .row.align-items-end
+      .col-lg-3.col-md-2
+        img.my-4(src='@/assets/curso/tema2/cientifico.png', alt='Métodos estándar para análisis de aguas en laboratorio')
+      .col-lg-6.col-md-8
+        img.my-4(src='@/assets/curso/tema2/standardMethods.png', alt='Métodos estándar para análisis de aguas en laboratorio')
+      .col-lg-3.col-md-2
+        img.my-4(src='@/assets/curso/tema2/cientifica.png', alt='Métodos estándar para análisis de aguas en laboratorio')
+
+    p.my-4 En cada uno de los apartados citados anteriormente se encuentra información sobre las técnicas de muestreo, incluyendo la preservación y el almacenamiento de aguas para cada uno de los parámetros analizados dentro de estos 10 tipos de ensayo.
+    p.mb-4 Puede consultar el resumen de los contenidos de cada apartado en la versión <em>web</em> disponible en el siguiente enlace:
+    .row.justify-content-center
+      .col-8
+        a.anexo.mb-4.mb-lg-0(href="https://www.standardmethods.org" target="_blank")
+          .anexo__icono
+            img.mt-4(src="@/assets/template/tema2/icono-link.svg")
+          .anexo__texto
+            p Enlace <em>web</em>. Para consultar las normas internacionales por las cuales también son regidos los ensayos de laboratorio que se realizan en el país, puede consultar los <em>Standard Methods for the examination of water and wastewater</em>.
+
+    h3 Normas ISO
+    p.my-4 Las normas ISO son otro referente importante para el muestreo y análisis microbiológico y fisicoquímico del agua, elaborados por la organización internacional de normalización ISO con sede en Ginebra suiza y conformada por más de 160 países miembros.
+
+    .row.justify-content-center.lineaPeq
+      .col-lg-7.p-4.col-md-10.fondo3
+        p.mb-3 Las normas son realizadas por comités técnicos, expertos en cada uno de los temas de normalización. En el caso del muestreo y análisis de agua, el comité técnico encargado es el ISO TC 146 y el subcomité 6 es el encargado de trabajar en guías y normas para el muestreo de aguas. El subcomité 4 es el encargado de trabajar específicamente en los aspectos relacionados con el análisis microbiológico de aguas.
+        p La serie de normas 5667 proporcionan directrices para la toma de muestras para el análisis de la calidad del agua. En el caso de muestreo para análisis microbiológico se recomienda consultar la ISO 19458.
+      .col-lg-3.col-md-10
+        img.imagenPeq(src="@/assets/curso/tema2/imagen6.jpg")        
+
+    .row.justify-content-center.my-5
+      .col-8
+        a.anexo.mb-4.mb-lg-0(href="https://ecored-sena.github.io/921200_CF3_AGUA_Y_SANAMIENTO/public/main.html#page/protocolos_de_laboratorio/" target="_blank")
+          .anexo__icono
+            img.mt-4(src="@/assets/template/tema2/icono-link.svg")
+          .anexo__texto
+            p Enlace <em>web</em>.
+            p Debe ingresar a biblioteca SENA, ir a ICONTEC, buscar norma NTC 5667 y escoger la que desee visualizar
+
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -2,24 +2,113 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
-    #t_3_1.pb-5.h2 tema 3.1
-    .pb-5(v-for="item of 15")
 
-    #t_3_2.pb-5.h2 tema 3.2
-    .pb-5(v-for="item of 15")
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 3
+      h1 Lineamientos técnicos de agua potable y saneamiento
 
-    #t_3_3.pb-5.h2 tema 3.3
-    .pb-5(v-for="item of 15")
+    .row.justify-content-center
+      .col-lg-7.col-md-12.p-4
+        p.mb-3.fondo4 Para le regulación del sector de potabilización del agua contamos con el reglamento técnico del sector de agua potable y saneamiento básico RAS, el cual es una herramienta creada por el ministerio de vivienda en Colombia para mejorar la calidad de la prestación de servicios en agua potable y saneamiento básico, estas guías tienen mucha información de diseño.
+        p En Colombia el uso de las guías y manuales del RAS es obligatorio para los sistemas de captación, conducción, almacenamiento y tratamiento de agua residual y potable según la resolución 0330 de 2017. Hasta el momento hay 14 manuales definidos por el gobierno sobre las buenas prácticas en el sector de saneamiento y agua en Colombia y a su vez están disponibles 8 guías para que las personas involucradas en desarrollo de proyectos en agua y saneamiento básico puedan desenvolverse correctamente.
+      .col-lg-5.col-md-12
+        img.my-4(src='@/assets/curso/tema3/imagen1.png')
 
-    #t_3_4.pb-5.h2 tema 3.4
-    .pb-5(v-for="item of 15")
+    .tarjeta--container.row.mb-5
+      .col-md.tarjeta.color-primario.p-5.fondo5.centrar
+        .row
+          .col-12
+            p.text-center.my-5 En Colombia el uso de las Guías y Manuales del RAS es obligatorio para los sistemas de captación, conducción, almacenamiento y tratamiento de agua residual y potable según la resolución 0330 de 2017.
 
-    #t_3_5.pb-5.h2 tema 3.5
-    .pb-5(v-for="item of 15")
+      .col-md.tarjeta.color-primario.p-5.fondo6.centrar
+        .row
+          .col-12
+            p.text-center.my-5 Hasta el momento hay 14 manuales definidos por el gobierno sobre las buenas prácticas en el sector de saneamiento y agua en Colombia.
 
-    #t_3_6.pb-5.h2 tema 3.6
-    .pb-5(v-for="item of 15")
+      .col-md.tarjeta.color-primario.p-5.fondo7.centrar
+        .row
+          .col-12
+            p.text-center.my-5 A su vez están disponibles 8 guías para que las personas involucradas en desarrollo de proyectos en agua y saneamiento básico podamos desenvolverse correctamente.
+    .row.fondo8.align-items-center
+      .col-lg-5.col-md-12
+        img.my-4(src='@/assets/curso/tema3/imagen2.png')
+      .col-lg-7.col-md-12.bordes
+        p.p-3 La resolución 0330 de 2017 reglamenta cuales son los requisitos técnicos que se deben cumplir en cada una de las etapas, de diseño, construcción, puesta en marcha, operación, mantenimiento y rehabilitación de aquella infraestructura que esté relacionada con los servicios de alcantarillado, acueducto y aseo.        
+      p.my-4 El contenido que maneja esta resolución 0330 de 2012 es el siguiente: 
+      .row.justify-content-center
+        .col-lg-8.col-md-10
+          PasosA.color-primario.mb-5.px-5(tipo="l")
+            div
+              p Se presentan las condiciones generales del reglamento técnico para los sectores de agua potable y saneamiento básico.
+
+            div
+              p Se especifican los requisitos técnicos: En esta resolución se esclarecen los procesos que se deben involucrar en el diseño, la construcción y previa supervisión de los sistemas de acueducto, alcantarillado y aseo que se desarrollen en el país.
+
+            div
+              p Se hace un control y régimen de sanciones del reglamento técnico del sector de agua potable y saneamiento básico.
+
+            div
+              p Se enumeran las certificaciones, licencias y permisos necesarios dentro del reglamento técnico del sector de agua potable y saneamiento básico.
+            div
+              p Definición de conceptos.
+      h3.my-4 Pruebas de tratabilidad
+      p.mb-4 Las pruebas de tratabilidad son utilizadas para definir el diseño adecuado de una planta de tratamiento de agua para potabilización dado que este es el método que más se utiliza para poder determinar y evaluar los procesos de coagulación y floculación.
+
+      .bloque-texto-g.p-3.p-sm-4.p-md-5.fondo9
+        .bloque-texto-g__img(
+          :style="{'background-image': `url(${require('@/assets/curso/tema3/imagen3.jpg')})`}"
+        )
+        .bloque-texto-g__texto.p-4
+          p.mb-0 El color y la turbiedad son algunas de las características del agua las cuales representan una mayor calificación dentro del Índice de Riesgo de la Calidad del Agua (IRCA), es por esto que los objetivos de una planta de tratamiento deben estar enfocados a obtener unas condiciones óptimas de tratabilidad del agua.
+      h3.my-4 NTC Prueba de jarras.
+      p.mb-4 Para la correcta realización de la prueba de jarras para la determinación de la correcta tratabilidad del agua se encuentra la Norma 
+        span.marcador <a href="https://pdfslide.net/documents/ntc-3903-ensayo-de-jarras-561824949d208.html" style="color: white; text-decoration-line: none;" target="_blank">Técnica Colombiana NTC 3903 </a> 
+        | procedimiento para el método de jarras en la coagulación-floculación del agua.
+
+    img.my-4(src='@/assets/curso/tema3/Norma.png', alt='Norma Técnica de Colombia')
+
+    .row
+      .col-lg-4.col-md-10.centrar
+        img.my-4(src='@/assets/curso/tema3/imagen4.png', alt='Cálculo de remoción')
+      .col-lg-7.col-md-12
+        h3.my-4 Cálculo de remoción
+        p.mb-4 Para poder obtener un cálculo acertado de la remoción de la carga contaminante en un sistema de tratamiento de aguas se debe utilizar la siguiente fórmula:
+        .row.justify-content-center
+          .col-lg-3.col-md-6.fondo10
+            p.text-white.text-center.mb-0 E = (So - S) / So x 100
+        p.my-4 En donde respectivamente:
+
+        .row
+          .col-5.ms-5
+
+            .row.my-4
+              .col-auto
+                a.boton.fondo11.indicador__container(@click="modal1 = true")
+                  span.text-white E
+                  .indicador--click(v-if="mostrarIndicador")
+            
+            ModalA(:abrir-modal.sync="modal1")
+              p Eficiencia de remoción del sistema.
+
+            .row
+              .col-auto
+                a.boton.fondo11.indicador__container(@click="modal2 = true")
+                  span.text-white S
+                  .indicador--click(v-if="mostrarIndicador")
+            
+            ModalA(:abrir-modal.sync="modal2")
+              p Carga contaminante de salida.
+
+            .row.my-4
+              .col-auto
+                a.boton.fondo11.indicador__container(@click="modal3 = true")
+                  span.text-white So
+                  .indicador--click(v-if="mostrarIndicador")
+            
+            ModalA(:abrir-modal.sync="modal3")
+              p Carga contaminante de entrada. 
+        p.my-4 A su vez, existen diversos factores que pueden afectar la eficiencia de la remoción, algunos de ellos pueden ser, el tiempo de residencia hidráulico, el medio de soporte, la configuración de los reactores, la temperatura y el pH.
 
 </template>
 
@@ -27,6 +116,9 @@
 export default {
   name: 'Tema3',
   data: () => ({
+    modal1: false,
+    modal2: false,
+    modal3: false,
     // variables de vue
   }),
   mounted() {
